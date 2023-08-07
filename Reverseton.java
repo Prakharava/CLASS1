c4f477ea65674da4d01c11d07daa59cbe27915e6
Prakharava <122719317+Prakharava@users.noreply.github.com>
2023-08-07 10:20:07 +0530

    Add files via upload

---
+++ Reverseton.java
@@ -0,0 +1,13 @@
+package College;
+import java.util.*;
+public class Reverseton {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        do{
+            System.out.println(n);
+            n--;
+        }
+        while(n>0);
+    }
+}
