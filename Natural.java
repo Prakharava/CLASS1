aa0800d402f76779dbad831a1159646796a11ef5
Prakharava <122719317+Prakharava@users.noreply.github.com>
2023-08-07 10:18:16 +0530

    Add files via upload

---
+++ Natural.java
@@ -0,0 +1,13 @@
+package College;
+import java.util.*;
+public class Natural {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a=0;
+        while(a<=n){
+            System.out.println(a);
+            a++;
+        }
+    }
+}
